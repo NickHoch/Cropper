5bf43297b78e04fd4ab3911853fe582f1041a015
kolyagoch <kolyagoch@step.itstep.rv.ua>
2018-11-22 09:55:42 +0200

    Add project files.

---
+++ Cropper/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Cropper.Startup))]
+namespace Cropper
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
